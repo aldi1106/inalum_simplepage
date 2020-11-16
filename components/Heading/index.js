fdc885e81096b5ff7cdd336ede13f4694712894d
Aldi Kesuma <aldikesuma1234@gmail.com>
2020-11-16 19:20:01 +0700

    first commit

---
+++ components/Heading/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+
+const Heading = ({text}) => {
+	return (
+	<View>
+         <Text style = {styles.text}>{text}</Text>
+	</View>
+)
+}
+
+const styles = StyleSheet.create({
+	text: {
+           fontSize: 24,
+		   color: '#2e2e2e'
+	}
+})
+Heading.propTypes = {
+    text: PropTypes.string.isRequired
+}
+
+export default Heading;
